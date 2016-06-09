5ed0f2e1fa0df395a72bfb2f2d39fcd4afe3f02f
David Von Lehman <david.vonlehman@gmail.com>
2016-06-09 11:38:57 -0700

    New 2.0 version that works off a .ebdeploy.yml config file

--- bin/ebdeploy.js
+++ bin/ebdeploy.js
@@ -1,35 +1,37 @@
 #!/usr/bin/env node
 
-var archiver = require('archiver');
 var path = require('path');
 var async = require('async');
 var fs = require('fs');
 var os = require('os');
 var ncp = require('ncp');
-var npmi = require('npmi');
 var AWS = require('aws-sdk');
 var debug = require('debug')('ebdeploy');
 var readdirp = require('readdirp');
-var rimraf = require('rimraf');
-var urljoin = require('url-join');
-var proxy = require('proxy-agent');
 var HttpsProxyAgent = require('https-proxy-agent');
-var spawn = require('child_process').spawn;
 var yargs = require('yargs').argv;
+var yaml = require('js-yaml');
+var winston = require('winston');
+var _ = require('lodash');
 
-validateArgs();
 
-var versionLabel = 'ebdeploy-' + Date.now();
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+  level: 'info',
+  prettyPrint: true,
+  colorize: true,
+  silent: false,
+  timestamp: false
+});
+var log = winston;
 
-if (yargs._.length === 0)
-  sourceDir = process.cwd();
-else
-  sourceDir = yargs._[0];
+var params = _.assign({}, yargs, {
+  sourceDir: process.cwd(),
+  versionLabel: 'ebdeploy-' + Date.now(),
+});
 
-var packageJson;
-
-var awsConfig = {
-  logger: process.stdout,
+params.awsConfig = {
+  // logger: process.stdout,
   region: yargs.region || 'us-west-2',
   maxRetries: 0,
   httpOptions: {
@@ -38,81 +40,86 @@ var awsConfig = {
 };
 
 if (yargs.profile) {
-  console.log("using AWS profile %s", yargs.profile);
-  awsConfig.credentials = new AWS.SharedIniFileCredentials({profile: yargs.profile});
+  log.info('using AWS profile %s', yargs.profile);
+  params.awsConfig.credentials = new AWS.SharedIniFileCredentials({profile: yargs.profile});
 }
 
 if (process.env.HTTPS_PROXY) {
-  console.log("setting http proxy to %s", process.env.HTTPS_PROXY);
-  awsConfig.httpOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
+  log.info('setting http proxy to %s', process.env.HTTPS_PROXY);
+  params.awsConfig.httpOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
 }
 
-console.log("updating AWS config");
-AWS.config.update(awsConfig);
+log.info('updating AWS config');
+AWS.config.update(params.awsConfig);
 
 var tasks = [];
 
-if (yargs.tempDir) {
-  tasks.push(createTempWorkingDirectory);
-  tasks.push(copyFiles);
-}
-else {
-  workingDir = sourceDir;
-}
+tasks.push(loadDeployConfig);
+tasks.push(createTempWorkingDirectory);
+tasks.push(copyFiles);
 
-var elasticbeanstalk = new AWS.ElasticBeanstalk();
-var s3 = new AWS.S3({
-  s3ForcePathStyle: true,
-  httpOptions: {
-    timeout: 60000
-  }
+// Register task to create the zip archive
+tasks.push(function(cb) {
+  require('../lib/zip')(params, cb);
 });
 
-var s3Key = yargs.s3Path + "/" + versionLabel + ".zip";
-
-tasks.push(loadPackageJson);
-tasks.push(deleteNodeModules);
-tasks.push(npmInstall);
-
-if (yargs.skipOptionalDependencies === true)
-  tasks.push(stripOptionalDependencies);
-
-tasks.push(revisedPackageJson);
-tasks.push(generateZipArchive);
-
-if (yargs.dryRun !== true) {
-  tasks.push(uploadArchiveToS3);
-  tasks.push(createElasticBeanstalkVersion);
-  tasks.push(updateBeanstalkEnvironmentVersion);
+// If not a dryrun, register task to deploy to EB
+if (params.dryRun !== true) {
+  tasks.push(function(cb) {
+    require('../lib/deploy')(params, cb);
+  });
 }
 
 async.series(tasks, function(err) {
   if (err) {
-    console.error(err);
+    log.error(err.message);
     process.exit(1);
-  }
-  else {
-    console.log("deployment %s complete", versionLabel);
+  } else {
+    log.info('Deployment %s complete to all environments', params.versionLabel);
     process.exit(0);
   }
 });
 
-function deleteNodeModules(callback) {
-  rimraf(path.join(workingDir, 'node_modules'), callback);
+function createTempWorkingDirectory(callback) {
+  params.workingDir = path.join(os.tmpdir(), params.versionLabel);
+  fs.mkdir(params.workingDir, callback);
 }
 
-function createTempWorkingDirectory(callback) {
-  workingDir = path.join(os.tmpdir(), versionLabel);
-  fs.mkdir(workingDir, callback);
+function loadDeployConfig(callback) {
+  // Look for a .ebdeploy.yml file in the workding dir
+  var config;
+  var yamlConfig = path.join(process.cwd(), '.ebdeploy.yml');
+  fs.readFile(yamlConfig, function(err, data) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return callback('No .ebdeploy.yml file found');
+      }
+      return callback(err);
+    }
+
+    try {
+      config = yaml.safeLoad(data, 'utf-8');
+    } catch (yamlErr) {
+      return callback(new Error('Invalid .ebdeploy.yml: ' + yamlErr.message));
+    }
+
+    if (!_.isArray(config.environments) || _.isEmpty(config.environments)) {
+      return callback(new Error('At least one ElasticBeanstalk environment must be specified'));
+    }
+
+    _.assign(params, config);
+
+    callback(null);
+  });
 }
 
 // Create a temporary directory and copy all the necessary files outside of the
 function copyFiles(callback) {
-  console.log("copying files to %s", workingDir);
+  log.info('copying files to %s', params.workingDir);
 
   // Copy everything except node_modules to the tempDirectory
   readdirp({
-    root: sourceDir,
+    root: params.sourceDir,
     directoryFilter: ['!.git', '!node_modules'],
     depth: 0,
     entryType: 'both'
@@ -121,246 +128,10 @@ function copyFiles(callback) {
 
     var all = res.files.concat(res.directories);
 
-    async.each(all, function(f, cb) {
-      ncp(f.fullPath, path.join(workingDir, f.path), cb);
+    async.each(all, function(fileInfo, cb) {
+      ncp(fileInfo.fullPath, path.join(params.workingDir, fileInfo.path), cb);
     }, callback);
   });
-}
-
-function loadPackageJson(callback) {
-  fs.readFile(path.join(workingDir, 'package.json'), function(err, data) {
-    if (err) return callback(err);
-
-    try {
-      packageJson = JSON.parse(data);
-    }
-    catch (err) {
-      return callback(new Error("Invalid package.json"));
-    }
-
-    callback();
-  });
-}
-
-function npmInstall(callback) {
-  console.log("running npm install");
-
-  var npmArgs = ['install', '--production'];
-  if (yargs.skipOptionalDependencies === true)
-    npmArgs.push('--no-optional');
-
-  var child = spawn('npm', npmArgs, {cwd: workingDir});
-  child.stdout.on('data', function(data) {
-    console.log(data.toString());
-  });
-
-  child.stderr.on('data', function(data) {
-    console.error(data.toString());
-  });
-
-  child.on('error', function(err) {
-    return callback(err);
-  });
-
-  child.on('close', function(code) {
-    if (code !== 0)
-      return callback(new Error("npm install did not exit normally"));
-
-    return callback();
-  });
-}
-
-// Even though we are shipping node_modules in the zip,
-// ElasticBeanstalk is still going to run npm install and
-// and we are not able to specify the --skip-optional flag.
-// So instead strip out all optionalDependency sections from
-// each package.json file.
-function stripOptionalDependencies(callback) {
-  console.log("Stripping optional dependencies from package.json files");
-
-  var opts = {
-    root: workingDir,
-    fileFilter: function(entry) {
-      return entry.name === 'package.json';
-    }
-  };
-
-  readdirp(opts, function(err, res) {
-    if (err) return callback(err);
-
-    async.each(res.files, function(f, cb) {
-      fs.readFile(f.fullPath, function(err, json) {
-        if (err) return cb(err);
-
-        var packageJson;
-        try {
-          packageJson = JSON.parse(json);
-        }
-        catch (err) {
-          return cb(new Error("File " + f.path + " is not valid JSON"));
-        }
-
-        if (!packageJson.optionalDependencies)
-          return cb();
-
-        // Delete the optionalDependencies section and save the file back
-        debug("stripping optionalDependencies from %s", f.path);
-        delete packageJson.optionalDependencies;
-        fs.writeFile(f.fullPath, JSON.stringify(packageJson, null, 2), cb);
-      });
-    }, callback);
-  });
-}
-
-// Write a modified package.json without any dependencies.
-function revisedPackageJson(callback) {
-  console.log("revised package.json without dependencies");
-
-  if (packageJson.scripts) {
-    // We've already performed the preinstall locally
-    delete packageJson.scripts.preinstall;
-  }
-
-  packageJson.dependencies = {};
-  packageJson.devDependencies = {};
-
-  fs.writeFile(path.join(workingDir, 'ebpackage.json'), JSON.stringify(packageJson, null, 2), callback);
-}
-
-function generateZipArchive(callback) {
-  var zipFile = path.join(workingDir, versionLabel + '.zip');
-  console.log("generating zip archive %s", zipFile);
-
-  var erroredOut = false;
-  var zipStream = fs.createWriteStream(zipFile);
-  var archive = archiver('zip');
-
-  // Make sure we don't include the zip file itself or the temporary ebpackage.json
-  var fileFilters = ['!' + versionLabel + '.zip', '!ebpackage.json'], directoryFilters = [];
-
-  fs.readFile(path.join(workingDir, ".ebignore"), function(err, ebIgnore) {
-    if (err) {
-      if (err.code === 'ENOENT')
-        return callback(new Error("No .ebignore file found"));
-      else
-        return callback(err);
-    }
-
-    // Read in the .ebinclude patterns
-    var globPatterns = ebIgnore.toString().split('\n');
-    globPatterns.forEach(function(pattern) {
-      // TODO: Disallow negation patterns in .ebignore
-      // Force all patterns to be negation patterns
-      // Directory filter patterns must end in a slash
-      if (pattern.slice(-1) === '/') {
-        directoryFilters.push('!' + pattern.slice(0, -1));
-      }
-      else {
-        fileFilters.push('!' + pattern);
-      }
-    });
-
-    var entryStream = readdirp({
-      root: workingDir,
-      fileFilter: fileFilters,
-      directoryFilter: directoryFilters
-    })
-    .on('data', function(entry) {
-      // Don't include the original package.json
-      if (entry.path === '/package.json')
-        return;
-
-      // Add the file to the zip archive
-      debug('writing entry %s to archive', entry.path);
-      archive.file(entry.fullPath, {name: entry.path});
-    })
-    .on('error', function(err) {
-      erroredOut = true;
-      return callback(err);
-    })
-    .on('end', function() {
-      // Write the revised ElasticBeanstalk package.json
-      debug("writing revised package.json");
-      archive.file(path.join(workingDir, 'ebpackage.json'), {name: 'package.json'});
-
-      debug("finalizing archive");
-      archive.finalize();
-    });
-
-    zipStream.on('close', function() {
-      console.log("done writing to zip archive");
-      callback();
-    });
-
-    archive.on('error', function(err) {
-      debug("archiver error");
-      if (erroredOut !== true)
-      callback(err);
-    });
-
-    archive.pipe(zipStream);
-  });
-}
-
-function uploadArchiveToS3(callback) {
-  console.log("uploading deploy zip to S3 with key %s", s3Key);
-
-  s3.putObject({
-    Key: s3Key,
-    Body: fs.createReadStream(path.join(workingDir, versionLabel + '.zip')),
-    Bucket: yargs.bucket
-  }, callback);
-}
-
-function createElasticBeanstalkVersion(callback) {
-  console.log("Creating ElasticBeanstalk version");
-
-  // TODO: Read this from the .elasticbeanstalk/config.yml file
-  var params = {
-    ApplicationName: yargs.appName,
-    VersionLabel: versionLabel,
-    AutoCreateApplication: false,
-    SourceBundle: {
-      S3Bucket: yargs.bucket,
-      S3Key: s3Key
-    }
-  };
-
-  elasticbeanstalk.createApplicationVersion(params, function(err, data) {
-    if (err) return callback(err);
-
-    console.log("create version data: %o", data);
-    callback();
-  });
-}
-
-function updateBeanstalkEnvironmentVersion(callback) {
-  console.log("deploying version %s to environment %s", versionLabel, yargs.environment);
-
-  var options = {
-    EnvironmentName: yargs.environment,
-    VersionLabel: versionLabel
-  };
-
-  elasticbeanstalk.updateEnvironment(options, callback);
-}
-
-function validateArgs() {
-  if (!yargs.region)
-    yargs.region = 'us-west-2';
-
-  var error;
-  if (!yargs.appName)
-    error = "Missing --app-name argument";
-  else if (!yargs.environment)
-    error = "Missing --app-name argument";
-  else if (!yargs.bucket)
-    error = "Missing --bucket argument";
-
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
 }
 
 // https://github.com/jsebfranck/elastic-beanstalk.js/blob/master/lib/awsClient.js
